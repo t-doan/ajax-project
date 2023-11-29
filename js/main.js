9703a39a9edeae8cf1c4d6a434b5f1a28d386064
Tai Doan <DoanTai11@gmail.com>
2023-11-29 02:46:45 +0000

    completed header styling #1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.rhodesapi.com/api/operator');
+// xhr.responseType = 'json';
+// xhr.addEventListener('load', function () {
+//   console.log(xhr.status);
+//   console.log(xhr.response);
+//   for (const user of xhr.response) {
+//     console.log('operator:', user);
+//     const newImg = document.createElement('img');
+//     if (user.art.length >= 2){
+//           const art = user.art[2].link;
+//           console.log('art:', art);
+//           newImg.setAttribute('src', art);
+//           main.appendChild(newImg);
+//     }
+//   }
+// });
+// xhr.send();
+
+// const main = document.querySelector('main');
